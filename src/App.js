06324189157df95ee4413aacc880c9090be1ba2e
jennyhuoh <huoh.li@gmail.com>
2021-05-31 22:01:23 +0800

    add service-worker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Burger from './pages/Buger';
+import Meat from './pages/Meat';
+import ShoppingCart from './pages/ShoppingCart';
+import Ingredients from './pages/Ingredients';
+import Complete from './pages/Complete';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StoreProvider } from "./store/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/shoppingCart" component={ShoppingCart} />
+          <Route exact path="/burger" component={Burger} />
+          <Route exact path="/meat" component={Meat} />
+          <Route exact path="/ingredients" component={Ingredients} />
+          <Route exact path="/complete" component={Complete} />
+        </Switch>
+      </BrowserRouter>
+    </StoreProvider>
   );
 }
 
